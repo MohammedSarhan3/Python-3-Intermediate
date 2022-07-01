ed078f95d1e2b2f3ea7e0ec69810d05c4db5a9d1
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-01 19:08:30 +0200

    functools reduce tool

--- 03-funcprograming.py
+++ 03-funcprograming.py
@@ -7,3 +7,22 @@ def mytitle(x):
     return x.title()
 new_names = map(mytitle,names)
 print(list(new_names))
+
+#filter
+numbers=[20,100,80,25,110,50,55,18]
+def bigger(x):
+    if x>50:
+        return True
+    else:
+        return False
+
+mynumbers=filter(bigger,numbers)
+print(list(mynumbers))
+
+
+#reduce
+numbers2=[20,100,80,25,110,50,55,18]
+def mysum(x,y):
+    return x+y
+result = reduce(mysum,numbers)
+print(result)
