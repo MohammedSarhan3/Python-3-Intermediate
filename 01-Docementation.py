9d82595ef6f667b2a27a117c5c7e8bfabc2abd6a
Mohammad Wael Sarhan <saraheeeen@gmail.com>
2022-07-01 18:51:23 +0200

    comment :  Developers

---
+++ 01-Docementation.py
@@ -0,0 +1,12 @@
+#readable
+#comment :  Developers
+
+class Clac:
+    def __init__(self):
+        print("Welcome")
+
+    def sum (self,x,y):
+        #return the sum of x,y
+        return x+y # BUG: return the sum of x,y
+                    #HOTFIX: return the sum of x,y
+                    #TODO: return the sum of x,y
